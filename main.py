94346b4e3df8ef80b56f38824722f1809e504f48
siddhant s <siddhantsaraf.91@gmail.com>
2025-05-16 11:52:40 +0530

    new crawler added not working though will have to fix and the text file created also commit but it hasn't any data yet

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import asyncio
 from crawl4ai import AsyncWebCrawler ,BrowserConfig, CrawlerRunConfig,CacheMode
+from crawl4ai.markdown_generation_strategy import DefaultMarkdownGenerator
+from crawl4ai.content_filter_strategy import PruningContentFilter
 
 async def main():
     browser_config = BrowserConfig(
@@ -7,12 +9,16 @@ async def main():
         browser_type="chromium",  # Use Chromium as the browser
 
     )
+    md_generator = DefaultMarkdownGenerator(
+        content_filter = PruningContentFilter(threshold=10)
+    )
     run_config = CrawlerRunConfig(
-        cache_mode=CacheMode.ENABLED,  # Ignore cache for fresh content
+        cache_mode=CacheMode.BYPASS,
+        markdown_generator=md_generator
     )
     async with AsyncWebCrawler(config= browser_config) as crawler:
-        result = await crawler.arun("https://www.myneta.info/",config=run_config)
-        print(result.markdown)  # Print first 300 chars
+        result = await crawler.arun("https://www.myneta.info/LokSabha2019/candidate.php?candidate_id=5560",config=run_config)
+        print(result.markdown.fit)
 
 if __name__ == "__main__":
     asyncio.run(main())
